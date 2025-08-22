8ad83c2e29ddc2b0bb53bd2c62da40c290b836b4
kiyuenter <133565523+kiyuenter@users.noreply.github.com>
2025-08-22 09:02:25 +0300

    Update Notebook.jsx

--- src/pages/Notebook.jsx
+++ src/pages/Notebook.jsx
@@ -370,8 +370,8 @@ const Notebook = () => {
                                     <option value="">Select Trigger</option>
                                     <option value="FVG">FVG</option>
                                     <option value="OB">OB</option>
-                                    <option value="Other">BB</option>
-                                    <option value="Other">MB</option>
+                                    <option value="BB">BB</option>
+                                    <option value="MB">MB</option>
                                     <option value="Liquidity Sweep">Liquidity Sweep</option>
                                     <option value="Other">Other</option>
                                 </select>
